a7725dcd47954d23be00e1171be21216946a6cc6
Krabs Burger <tudakudanado@gmail.com>
2023-01-03 16:10:59 +0400

    Created the main layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
+    return (
+        <div>
+            <header>
+                <div>Container for header
+                    <div>Logo</div>
+                    <div>Navigation
+                        <ul>
+                            <li>1 menu item</li>
+                            <li>2 menu item</li>
+                            <li>3 menu item</li>
+                            <li>4 menu item</li>
+                        </ul>
+                    </div>
+                    <div>Burger</div>
+                </div>
+            </header>
+            <main>
+                <section>Medical massage</section>
+                <section>Thai massage</section>
+                <section>Relax massage</section>
+                <section>Stone therapy</section>
+            </main>
+            <footer>
+                <div>Footer name or something</div>
+                <div>Social and contacts
+                    <ul>
+                        <li>Social icon 1</li>
+                        <li>Social icon 2</li>
+                        <li>Social icon 3</li>
+                    </ul>
+                </div>
+            </footer>
+        </div>
+    )
 }
 
 export default App;
